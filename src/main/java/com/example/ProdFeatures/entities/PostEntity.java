8a5bb9287968fafc7b10062927b803f11eb2c1dd
sushil-18 <sushilkumarwaghmare9@gmail.com>
2024-08-18 23:42:10 +0530

    Keeping the streak

--- src/main/java/com/example/ProdFeatures/entities/PostEntity.java
+++ src/main/java/com/example/ProdFeatures/entities/PostEntity.java
@@ -4,11 +4,17 @@ package com.example.ProdFeatures.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "posts")
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class PostEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,4 +23,11 @@ public class PostEntity {
     private String title;
 
     private String description;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
 }
